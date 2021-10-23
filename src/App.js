ca49da63207d937ba27c8b126dc1347b28b58673
sujaymec <sujay.mec@gmail.com>
2021-10-24 01:21:45 +0530

    converting to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Landingpage from './components/LandingPage';
 import './App.sass';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component{
+
+  render(){
+    return    <
+    div className="App">
       <Landingpage />
     </div>
-  );
+  }
 }
 
-export default App;
